61b259a2a8a4e28c9fc70e21d8eb29988857a45a
Hugo Porto <hugoporto.ti@gmail.com>
2021-06-11 01:28:11 -0300

    substituição dos arquivos index.html e inclusão da pasta e arquivo js

---
+++ js/main.js
@@ -0,0 +1,42 @@
+//alert("Meu Primeiro JS")
+	var nome1 ="José";
+	var nome2 ="Maria";
+	var idade1 =29;
+	var idade2 = 10;
+	var soma=(idade1+idade2);
+	var frase = "Brasil é melhor time do mundo";
+
+/*Criação de variáveis e uso do alert
+	alert(nome1 + " "+ nome2+" " +(idade1+idade2));
+	console.log(nome1);
+	console.log(soma);
+	console.log(frase);
+	console.log(frase.toUpperCase());*/
+
+//Ações com lista
+	var lista = ["macã","pêra","laranja"];
+	lista.push("banana");
+	console.log(lista);
+
+//Datas
+	nome1 =prompt("Digite seu nome: ");
+	
+	alert("Homem = 1"+"\n"+"e Mulher = 2");
+	sexo=prompt("Digite seu sexo: ");
+	
+	//alert(typeof sexo);
+		
+	/*if(sexo==1){
+		alert("Seja bem vindo "+nome1);
+	}else{
+		alert("Seja bem vinda "+nome1);}*/
+	
+	switch(sexo){
+		case "1":	alert("Seja bem vindo "+nome1);
+		break;
+		case "2":	alert("Seja bem vinda "+nome1);
+		break;
+		default: alert("Escolha uma opção: ")
+	}
+	var tempo = new Date();
+	alert("Hoje é dia: " + tempo.getDate()+ " / " + ( tempo.getMonth()+1)+ " / " +(tempo.getYear()+1900));
